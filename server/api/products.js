4418e5c228891806526f40be051c70d301edaf4c
Isabel Hirama <isabel.hirama@gmail.com>
2018-07-24 17:38:37 -0400

    wrote api route to get one product by id

--- server/api/products.js
+++ server/api/products.js
@@ -4,9 +4,20 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    console.log('trying all products api')
+    console.log('starting all products api call')
     const products = await Product.findAll()
     res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    console.log('starting single products api call')
+    const id = req.params.productId
+    const product = await Product.findById(id)
+    res.json(product)
   } catch (err) {
     next(err)
   }
