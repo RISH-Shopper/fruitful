cbbf9c7467b061ae0d790e4b82ba4aa656cc4767
Hawa Sako <Hawa.d.sako@gmail.com>
2018-07-24 16:14:39 -0400

    updatesIssue4

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import Products from './Products'
+//import thunks from store once created
+
+class SingleProduct extends React.Component{
+	constructor(){
+		super();
+		this.state = {quantity:''}
+	}
+}
+
+
+
