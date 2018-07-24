c39770085d199ebecef3dd1e5e9cfe5c272976a7
Isabel Hirama <isabel.hirama@gmail.com>
2018-07-24 15:53:52 -0400

    Completed api route for all products

--- server/api/products.js
+++ server/api/products.js
@@ -1,13 +1,17 @@
-const router = require('express').Router
-const {Product} = require('../db/models')
+const router = require('express').Router()
+const {Product, User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
+    console.log('trying all products api')
     const products = await Product.findAll()
     res.json(products)
   } catch (err) {
     next(err)
   }
 })
+
+
+
 
