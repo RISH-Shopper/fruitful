609cce51d35600e4b862078bfd941898fdf703e3
Rebecca Eom <mye391@gmail.com>
2018-07-27 19:41:07 -0400

    feat(orders): add redux store for AllOrders component

--- client/store/index.js
+++ client/store/index.js
@@ -3,11 +3,12 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import order from './order'
 import products from './product'
 import categories from './category'
 import cart from './cart'
 
-const reducer = combineReducers({user, products, categories, cart})
+const reducer = combineReducers({user, products, categories, cart, order})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -18,3 +19,4 @@ export * from './user'
 export * from './product'
 export * from './category'
 export * from './cart'
+export * from './order'
