03784c3428b62a0160f5ad5768924afde3b8d6ce
Rebecca Eom <mye391@gmail.com>
2018-07-27 00:33:08 -0400

    feat(delete): add to redux store

--- client/store/product.js
+++ client/store/product.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 const SET_PRODUCTS = 'SET_PRODUCTS'
 const ADD_PRODUCT = 'ADD_PRODUCT'
 const EDIT_PRODUCT = 'EDIT_PRODUCT'
+const DELETE_PRODUCT = 'DELETE_PRODUCT'
 
 /**
  * INITIAL STATE
@@ -26,6 +27,11 @@ export const addProduct = product => ({type: ADD_PRODUCT, product})
 
 export const editProduct = product => ({type: EDIT_PRODUCT, product})
 
+export const deleteProduct = product => ({
+  type: DELETE_PRODUCT,
+  productId: product.id
+})
+
 /**
  * THUNK CREATORS
  */
@@ -35,29 +41,49 @@ export const getProducts = () => {
       const {data} = await axios.get('/api/products');
       const action = setProducts(data);
       dispatch(action);
+    } catch (err) {
+      console.log(err)
     }
-    catch (error){
-      console.log(error)
-    }
-  };
-};
+  }
+}
 
 export const createProduct = (product, history) => {
     return async (dispatch) => {
-      const {data} = await axios.post('/api/products', product)
-      const action = addProduct(data)
-      dispatch(action)
-      history.push(`/products/${data.id}`)
+      try {
+        const {data} = await axios.post('/api/products', product)
+        const action = addProduct(data)
+        dispatch(action)
+        history.push(`/products/${data.id}`)
+      } catch (err) {
+        console.log(err)
+      }
     }
 }
 
 export const updateProduct = (product, productId, history) => {
   return async (dispatch) => {
-    const {data} = await axios.put(`/api/products/${productId}`, product)
-    console.log('DATA-----', data)
-    const action = editProduct(data)
-    dispatch(action)
-    history.push(`/products/${productId}`)
+    try {
+      const {data} = await axios.put(`/api/products/${productId}`, product)
+      console.log('DATA-----', data)
+      const action = editProduct(data)
+      dispatch(action)
+      history.push(`/products/${productId}`)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export const removeProduct = (productId, history) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.delete(`/api/products/${productId}`, productId)
+      const action = deleteProduct(data)
+      dispatch(action)
+      history.push('/products')
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
@@ -71,12 +97,12 @@ export default function(state = initialState, action) {
     case ADD_PRODUCT:
       return { ...state, products: [...state.products, action.product] }
     case EDIT_PRODUCT:
-      console.log('action.product', action.product)
-      console.log('state.products', state.products)
       return { ...state, products: [...state.products].map(product => {
         if (product.id === action.product.id) return action.product
         else return product
       }) }
+    case DELETE_PRODUCT:
+      return { ...state, products: state.products.filter(product => product.id !== action.productId) }
     default:
       return state
   }
