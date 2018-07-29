99e8633f61be5f98f3b7adc69c984b992a1bf061
Sarah Lynch <sarah@alum.rpi.edu>
2018-07-28 21:48:27 -0400

    Stripe integration on checkout

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,135 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Elements, StripeProvider} from 'react-stripe-elements'
+import CheckoutForm from './CheckoutForm'
 
 class Checkout extends Component {
-  constructor () {
+  constructor() {
     super()
-    this.state = {
-      email: '',
-      fullName: '',
-      address1: '',
-      address2: '',
-      city: '',
-      state: '',
-      zipcode: '',
-      country: '',
-      phoneNumber: ''
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange (evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value
-    })
-  }
-
-  handleSubmit (evt) {
-    evt.preventDefault()
-    const { email, fullName, address1, address2, city, state, zipcode, country, phoneNumber } = this.state
-
-  }
-
-  render () {
+  render() {
     return (
       <div>
-        <h2>Checkout</h2>
-        <form>
-          <div>
-          <label htmlFor='form'>Email</label>
-            <div>
-              <input
-                type='text'
-                name='email'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <h3>Shipping Address</h3>
-          <div>
-            <label htmlFor='form'>Full Name</label>
-            <div>
-              <input
-                type='text'
-                name='fullName'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>Address line 1</label>
-            <div>
-              <input
-                type='text'
-                name='address1'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>Address line 2</label>
-            <div>
-              <input
-                type='text'
-                name='address2'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>City</label>
-            <div>
-              <input
-                type='text'
-                name='city'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>State/Province/Region</label>
-            <div>
-              <input
-                type='text'
-                name='state'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>Zip Code</label>
-            <div>
-              <input
-                type='text'
-                name='zipcode'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>Country</label>
-            <div>
-              <input
-                type='text'
-                name='country'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor='form'>Phone Number</label>
-            <div>
-              <input
-                type='text'
-                name='phoneNumber'
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
-          <span>
-            <button type='submit'>Submit</button>
-          </span>
-        </form>
+        <StripeProvider apiKey="pk_test_6o0z0rTuKNakg2e2aU3iPolu">
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </StripeProvider>
       </div>
     )
   }
