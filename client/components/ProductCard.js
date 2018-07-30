f3168dad7c4a0c6608548cf4804d006a3172cea8
Hawa Sako <Hawa.d.sako@gmail.com>
2018-07-30 16:48:26 -0400

    StylingUpdateToProductPage

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-const ProductCard = (props) => {
-  const product = props.product
+const ProductCard = props => {
+	const product = props.product
 
-  return (
-    <div id='product' key= {product.id} className='column'>
-    <div className="h-100 card text-primary bg-light mb-3" style={{width: '22rem'}}>
-      <h5 class="card-title primary">{product.title}</h5>
-      <Link to={`/products/${product.id}`}>see more </Link>
-      <div>
-        <img className="card-img-top" src={product.photo} alt='Card image cap' />
-       </div>
-      </div>
-    </div>
-  )
+	return (
+		<div id="product" key={product.id} className="column">
+			<div className="card border-light mb-3" style={{width: '22rem'}}>
+				<h5 class="card-title primary">{product.title}</h5>
+				<Link to={`/products/${product.id}`}>see more </Link>
+				<div>
+					<img
+						className="card-img-top"
+						src={product.photo}
+						alt="Card image cap"
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
-
-
-
-
 
 export default ProductCard
