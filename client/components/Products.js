7ab016e01d5cd07877eb3f42b3e8edd059292565
Hawa Sako <Hawa.d.sako@gmail.com>
2018-07-30 23:18:39 -0400

    Incremental search implemented

--- client/components/Products.js
+++ client/components/Products.js
@@ -3,24 +3,22 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import ProductCard from './ProductCard'
 import Sidebar from './Sidebar'
-import { getProducts } from '../store/product'
-
+import {getProducts} from '../store/product'
 
 class Products extends Component {
-  componentDidMount () {
+  componentDidMount() {
     this.props.getProducts()
   }
 
-
-  filterByCategory = products => ( products.filter( product => {
-    const productCategories = product.categories.map(category => category.id)
-    return productCategories.includes(+this.props.categories.selectedCategory)
+  filterByCategory = products =>
+    products.filter(product => {
+      const productCategories = product.categories.map(category => category.id)
+      return productCategories.includes(+this.props.categories.selectedCategory)
     })
-  )
 
-  render () {
+  renderProductList = () => {
     // Needs to be "let" rather than const so that we can change when filtering
-    let { products } = this.props
+    let {products} = this.props
 
     // Filters by category if there is a selected category
     const categories = this.props.categories.categories
@@ -29,50 +27,60 @@ class Products extends Component {
       products = this.filterByCategory(products)
     }
 
-    const selectCategoryName = (selectedCategory
-                              ? categories.find(cat => +cat.id === +selectedCategory).title
-                              : 'All Products')
+    const selectCategoryName = selectedCategory
+      ? categories.find(cat => +cat.id === +selectedCategory).title
+      : 'All Products'
 
-    if (!products.length) {
-      return (
-        <div>
-          <p>There are currently no available products.</p>
-        </div>
-      )
+    console.log(this.props)
+    if (!this.props.products.length) {
+      return <p>There are currently no available products.</p>
     } else {
       return (
-        <div className= "products">
-          <Sidebar />
-          <div>
-            <div class="shadow-lg p-3 mb-5 bg-light rounded font-weight-normal text-center">
-              <h3>Currently viewing: {selectCategoryName}</h3>
-            </div>
-            <ul className = "card-group">
-              {products.map(product => (
-                <ProductCard product={product} key={product.id} />
-              ))}
-            </ul>
+        <div>
+          <div class="shadow-lg p-3 mb-5 bg-light rounded font-weight-normal text-center">
+            <h3>Currently viewing: {selectCategoryName}</h3>
           </div>
+          <ul className="card-group">
+            {products.map(product => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </ul>
         </div>
       )
     }
   }
+
+  render() {
+    return (
+      <div className="products">
+        <Sidebar />
+        {this.renderProductList()}
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-    products: state.products.products,
+const mapStateToProps = state => {
+  let foundProductIds = state.search.foundProductIds
+  let products;
+  if (foundProductIds.length === 0) {
+    products = state.products.products
+  } else {
+    products = state.products.products.filter(product =>
+      foundProductIds.includes(product.id)
+    )
+  }
+
+  return {
+    products: products,
     categories: state.categories
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     getProducts: () => dispatch(getProducts())
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Products)
+export default connect(mapStateToProps, mapDispatchToProps)(Products)
